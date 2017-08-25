194ce0480f0ce616d6e772755b070a7ac14b78ac
Sebastian Brudzinski <sbrudzinski@soldevelo.com>
2017-08-25 13:13:18 +0200

    Update RequisitionStatus to contain REJECTED status

--- src/main/java/mw/gov/health/lmis/mwrequisition/dto/RequisitionStatus.java
+++ src/main/java/mw/gov/health/lmis/mwrequisition/dto/RequisitionStatus.java
@@ -19,17 +19,28 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public enum RequisitionStatus {
   INITIATED(1),
+  REJECTED(1),
   SUBMITTED(2),
   AUTHORIZED(3),
-  IN_APPROVAL(4),
-  APPROVED(5),
-  RELEASED(6),
+  IN_APPROVAL(3),
+  APPROVED(4),
+  RELEASED(5),
   SKIPPED(-1);
 
   private int value;
 
   RequisitionStatus(int value) {
     this.value = value;
+  }
+
+  @JsonIgnore
+  public boolean isSubmittable() {
+    return value == 1;
+  }
+
+  @JsonIgnore
+  public boolean isUpdatable() {
+    return value < 4 && value != -1;
   }
 
   @JsonIgnore
@@ -44,10 +55,14 @@ public enum RequisitionStatus {
 
   @JsonIgnore
   public boolean isApproved() {
-    return value >= 5;
+    return value >= 4;
   }
 
   public boolean duringApproval() {
-    return value == 3 || value == 4;
+    return value == 3;
+  }
+
+  public boolean isAuthorized() {
+    return value >= 3;
   }
 }
